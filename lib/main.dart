d52dc060b12b298ebfd1cf2c69e3046c5ea64c6a
polinakhorkova <polinakhorkova@mail.ru>
2023-10-15 16:14:00 +0500

    second

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                padding: const EdgeInsets.all(12),
+               padding: const EdgeInsets.all(1), 
                 width: 350,
                 height: 482.233,
                 decoration: BoxDecoration(
@@ -43,14 +43,109 @@ class MyPage extends StatelessWidget {
                   children: [
                     Container(
                       width: 250,
-                      padding: const EdgeInsets.all(10.0),
-                      child: const Text(
-                        "ПОЛИТЕХ",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Color.fromRGBO(106, 111, 140, 1),
-                            fontSize: 30,
-                            fontWeight: FontWeight.w900),
+                      padding: const EdgeInsets.all(25.0),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "ПОЛИТЕХ",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Color.fromRGBO(106, 111, 140, 1),
+                                fontSize: 30,
+                                fontWeight: FontWeight.w900),
+                          ),
+                          Container(
+                            width: 500,
+                            padding: const EdgeInsets.all(15.0),
+                            height: 55,
+                            
+                             decoration: BoxDecoration(
+                            color: Color.fromRGBO(204, 229, 255, 1),
+                             borderRadius: BorderRadius.circular(0),
+                            border: Border.all(
+                              color: Color.fromRGBO(97, 154, 219, 1),
+                                width: 1),),
+                              
+                             
+                         
+                            child: Center(
+                              child: const Text(
+                                'Личный кабинет',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Color.fromRGBO(0, 76, 153, 1),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(16),
+                            child: Row(
+                              children: [
+                                Text(
+                                  'ЛОГИН',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w900,
+                                    color: Color.fromRGBO(192, 192, 192, 1),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 400,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(237, 236, 236, 1),
+                            borderRadius: BorderRadius.circular(30),
+                            border: Border.all(
+                              color: Color.fromRGBO(192, 192, 192, 1),
+                              width: 2,
+                              
+                            ),
+                          ),
+
+                          ),
+                             Container(
+                            padding: EdgeInsets.all(16),
+                            child: Row(
+                              children: [
+                                Text(
+                                  'ПАРОЛЬ',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w900,
+                                    color: Color.fromRGBO(192, 192, 192, 1),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                            Container(
+                            width: 400,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(237, 236, 236, 1),
+                            borderRadius: BorderRadius.circular(30),
+                            border: Border.all(
+                              color: Color.fromRGBO(192, 192, 192, 1),
+                              width: 2,
+                              
+                            ),
+                          ),
+
+                          ),
+                       Container(    
+                        
+                       ),
+
+                              
+                            
+                        
+                        ],
                       ),
                     ),
                   ],
